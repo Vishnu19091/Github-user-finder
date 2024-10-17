3e0d41f9483259b5c3b4e5795457ac9cce765bc5
Vishnu_1909 <143880481+Vishnu19091@users.noreply.github.com>
2024-10-17 08:53:48 +0530

    Update get_git_hub_user.py

--- get_git_hub_user.py
+++ get_git_hub_user.py
@@ -3,6 +3,7 @@ import requests
 import pandas as pd
 
 user=input('Enter User name:').strip()
+actions=input('Do you want to download the user details as csv file?(y/n)')
 
 gurl=f'https://api.github.com/users/{user}'
 
@@ -12,7 +13,10 @@ def get_user():
     if data.status_code!=404:
         datas=data.json()
         df=pd.DataFrame(datas.items())
-        df=df.rename(columns={0:"Content",1:"Values"})
+        df.columns=['Key','Values']
+        if actions=='y' or 'Y':
+            df.to_csv('GitHub_user_details.csv',index=False)
+        else: exit
 
     else:
         print("Not Found! Try another name")
